530fe23bf820cef67b32c77f3722961bdbe5187f
NerusuPavani37 <nerusupavani3737@gmail.com>
2025-02-13 20:34:16 +0530

    added update and delete methods

--- contollers/todoController.js
+++ contollers/todoController.js
@@ -5,6 +5,7 @@ exports.createTodo = async(req,res) =>{
   try{
      const todo = new Todo({title, user : req.user.id});
      await todo.save();
+     console.log("Saved Todo:", todo);
      res.status(201).json(todo);
   }catch(err){
     console.log(err);
@@ -20,3 +21,48 @@ exports.getTodos = async(req,res) =>{
     res.status(500).json({ msg: "Server error",err });
   }
 }
+
+exports.updateTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, completed } = req.body;
+
+    let todo = await Todo.findById(id);
+    if (!todo) {
+      return res.status(404).json({ msg: "Todo not found" });
+    }
+
+    if (todo.user.toString() !== req.user.id) {
+      return res.status(403).json({ msg: "Not authorized to update this todo" });
+    }
+
+    todo.title = title || todo.title;
+    todo.completed = completed !== undefined ? completed : todo.completed;
+
+    await todo.save();
+    console.log("Saved Todo:", todo);
+    res.json(todo);
+  } catch (err) {
+    res.status(500).json({ msg: "Server error", error: err.message });
+  }
+};
+
+exports.deleteTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    let todo = await Todo.findById(id);
+    if (!todo) {
+      return res.status(404).json({ msg: "Todo not found" });
+    }
+
+    if (todo.user.toString() !== req.user.id) {
+      return res.status(403).json({ msg: "Not authorized to delete this todo" });
+    }
+
+    await todo.deleteOne();
+    res.json({ msg: "Todo deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ msg: "Server error", error: err.message });
+  }
+};
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express")
-const {createTodo, getTodos} = require("../contollers/todoController");
+const {createTodo, getTodos, updateTodo, deleteTodo} = require("../contollers/todoController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createTodo);
 router.get("/",authMiddleware,getTodos);
+router.put("/:id", authMiddleware, updateTodo); 
+router.delete("/:id", authMiddleware, deleteTodo);
 
 module.exports = router;
