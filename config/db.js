e5254055d2832838faa10c779feda35eb801a8ef
NerusuPavani37 <nerusupavani3737@gmail.com>
2025-02-21 18:46:54 +0530

    Moved files to new folder

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-        await mongoose.connect(process.env.MONGO_URI)
-        .then(() => console.log("Connected to MongoDB"))
-        .catch((error) => console.error("MongoDB Connection Error:", error));
-};
-
-module.exports = connectDB;
